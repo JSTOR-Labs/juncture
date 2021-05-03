0064aa0e9870b0d1c5e1ffa3b87be1c5a2774d62
Ron Snyder <ron@snyderjr.com>
2021-05-03 19:52:39 -0400

    iiif server updates

--- utils/iiif-server/gcr-deploy.sh
+++ utils/iiif-server/gcr-deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# GCP_PROJECT='visual-essay'
 GCP_PROJECT='juncture-digital'
 
 GCR_SERVICE=${1:-iiif}
@@ -9,10 +8,10 @@ gcloud config set project ${GCP_PROJECT}
 gcloud config set compute/region us-central1
 gcloud config set run/region us-central1
 
-rm -rf build
-mkdir build
-rsync -va --exclude=venv --exclude=__pycache__ app.py atlas-creds iiifhosting-creds Dockerfile build
+rm -rf tmp
+mkdir tmp
+rsync -va --exclude=venv --exclude=__pycache__ app.py config.yaml Dockerfile tmp
 
-cd build
+cd tmp
 gcloud builds submit --tag gcr.io/${GCP_PROJECT}/${GCR_SERVICE}
 gcloud beta run deploy ${GCR_SERVICE} --image gcr.io/${GCP_PROJECT}/${GCR_SERVICE} --allow-unauthenticated --platform managed --memory 1Gi
