9d623ea8051325ffa846fd6c7e56aa918adb6a45
TomCampbell-Oulton <84871228+TomCampbell-Oulton@users.noreply.github.com>
2021-08-24 15:26:35 +0100

    Update Compare.vue

--- components/Compare.vue
+++ components/Compare.vue
@@ -97,7 +97,7 @@ module.exports = {
             const tileSource = manifest.sequences[0].canvases[manifest.seq || 0].images[0].resource.service
               ? `${manifest.sequences[0].canvases[manifest.seq || 0].images[0].resource.service['@id']}/info.json`
               : { url: manifest.sequences[0].canvases[manifest.seq || 0].images[0].resource['@id'] || manifest.metadata.find(md => md.label === 'source').value,
-                 type: 'image', buildPyramid: true }
+                 type: 'image', buildPyramid: true, TiledImage.setClip(50, 80) }
             return tileSource
           })
         })
