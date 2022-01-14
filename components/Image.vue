320ada839ab095df89728a9dcfe07ec8cf1f0f69
julia-ha <juliaxha@gmail.com>
2022-01-14 15:12:25 -0500

    Update Image.vue

--- components/Image.vue
+++ components/Image.vue
@@ -455,7 +455,7 @@ module.exports = {
     saveAnnotations() {
       this.annotations = this.annotator.getAnnotations()
       // console.log('saveAnnotations', this.annotations)
-      this.putFile(`${this.mdDir}${this.currentItemSourceHash}.json`, JSON.stringify(this.annotations, null, 2))
+      this.putFile(`${this.mdDir}/${this.currentItemSourceHash}.json`, JSON.stringify(this.annotations, null, 2))
     },
     annotationSelected(anno) {
       // console.log('annotationSelected', anno)
