cd41ed4f5d2a25ad6297ad40e0747353f56946bf
Ron Snyder <ronald.snyder@ithaka.org>
2022-02-11 08:56:49 -0500

    Update NowAndThen.vue

--- custom/components/NowAndThen.vue
+++ custom/components/NowAndThen.vue
@@ -13,7 +13,7 @@
 
 const dependencies = [
   'https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/openseadragon.min.js',
-  'https://cuberis.github.io/openseadragon-curtain-sync/src/openseadragon-curtain-sync.min.js'
+  'https://jstor-labs.github.io/juncture/js/openseadragon-curtain-sync.min.js'
 ]
 const iiifService = 'https://iiif.juncture-digital.org'
 const prefixUrl = 'https://openseadragon.github.io/openseadragon/images/'
